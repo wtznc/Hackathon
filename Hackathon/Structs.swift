e0da9ee81710e9390a33a53b7d65a7c8954da840
Wojciech Tyziniec <w.tyziniec@icloud.com>
2015-11-20 20:44:49 +0100

    dodanie socket.io, wykorzystanie do wysyłania i odbierania zapytan z serwera, dodanie parsowanie jsona

---
+++ Hackathon/Structs.swift
@@ -0,0 +1,21 @@
+//
+//  MessageStruct.swift
+//  Hackathon
+//
+//  Created by Wojciech Tyziniec on 20/11/15.
+//  Copyright © 2015 Wojciech Tyziniec. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    var msg = ""
+    var lang = ""
+    var from = ""
+}
+
+struct Greetings {
+    var from = ""
+    var lang = ""
+}
+
